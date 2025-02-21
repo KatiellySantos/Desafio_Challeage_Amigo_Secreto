2c2641f171f56192be931a3b7843a71c2bb99151
Katielly Santos <katygamesof@gmail.com>
2025-02-21 17:20:54 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const nomes = [];
+
+function adicionarAmigo() {
+    const nome = document.getElementById("amigo").value.trim();
+    if (nome) {
+        nomes.push(nome);
+        document.getElementById("amigo").value = "";
+        document.getElementById("listaAmigos").innerHTML += `<li>${nome}</li>`;
+    } else {
+        alert("Insira um nome válido.");
+    }
+}
+
+function sortearAmigo() {
+    const resultado = document.getElementById("resultado");
+    resultado.textContent = nomes.length ? `O amigo secreto é: ${nomes[Math.floor(Math.random() * nomes.length)]}` : "Lista vazia.";
+}
+
